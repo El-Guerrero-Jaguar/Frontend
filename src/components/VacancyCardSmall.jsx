2e8976d3ef221502f0bbef7000cb00e67775e4e9
Tonalli López <aguilablanca_7@live.com>
2020-12-18 14:06:19 -0600

    [Add] component VacancyCardSmall

---
+++ src/components/VacancyCardSmall.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../assets/styles/components/VacancyCard.scss';
+
+const VacancyCardSmall = ({ vacancy }) => (
+  <Link className="VacancyCard-container-small" to={`/vacacies/applied/${vacancy.id}`}>
+    <h4 className="VacancyCard-title">{vacancy.title}</h4>
+    <span className="VacancyCard-modality">
+      {vacancy.modality}
+    </span>
+  </Link>
+);
+
+export default VacancyCardSmall;
