6e178c16150aa04a601d3837c32ca957ae18caae
Tonalli López <aguilablanca_7@live.com>
2020-12-18 15:54:26 -0600

    [Add] HeaderVacancy to Vacancy and VacancyStatus

--- src/pages/VacancyStatus.jsx
+++ src/pages/VacancyStatus.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useAppContext } from '../context';
 
+import HeaderVacancy from '../components/HeaderVacancy';
+
 const VacancyStatus = () => {
   const [{ isCoach }] = useAppContext();
 
   return (
-    <h1>{isCoach ? 'Estado de la vacante del lado del coach' : 'Estado de la vacante del lado del alumno'}</h1>
+    <>
+      <HeaderVacancy isCoach={isCoach} />
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </>
   );
 };
 
