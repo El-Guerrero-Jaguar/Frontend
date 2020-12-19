4ba36222de19a33c9fe6eb613d590b78a29e763a
Tonalli López <aguilablanca_7@live.com>
2020-12-18 20:47:15 -0600

    [Update] Home and Home styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { useAppContext } from '../context';
+import withVacancies from '../hoc/withVacancies';
 
-const Home = () => {
+import VacanciesList from '../containers/VacaciesList';
+import StudentsList from '../containers/StudentsList';
+
+const Home = ({ vacancies }) => {
   const [{ isCoach }] = useAppContext();
+  const [search, setSearch] = useState('');
 
-  return (
-    <h1>{isCoach ? 'Home coach' : 'Home Alumn'}</h1>
+  const filtederVacancies = useMemo(() => vacancies.filter((vacancy) => (
+    vacancy.title.toLowerCase().includes(search.toLowerCase())
+  )), [vacancies, search]);
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return isCoach ? (
+    <>
+      <section className="Home-list-vacancies">
+        <h2>Lista de Vacantes</h2>
+        <VacanciesList vacancies={vacancies} />
+      </section>
+      <section className="Home-list-alumns">
+        <h2>Lista de Vacantes</h2>
+        <StudentsList />
+      </section>
+    </>
+  ) : (
+    <>
+      <div className="Home-search">
+        <input type="text" placeholder="Buscar vacante" onChange={handleSearch} />
+      </div>
+      <VacanciesList vacancies={filtederVacancies} />
+    </>
   );
 };
 
-export default Home;
+export default withVacancies(Home);
