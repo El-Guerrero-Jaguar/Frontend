0e720fec5df144c0557078de2db196bde3c71a24
Tonalli López <aguilablanca_7@live.com>
2020-12-19 00:34:29 -0600

    [Update] Vacancy and VacancyStatus

--- src/pages/VacancyStatus.jsx
+++ src/pages/VacancyStatus.jsx
@@ -9,7 +9,7 @@ const VacancyStatus = () => {
   return (
     <>
       <HeaderVacancy isCoach={isCoach} />
-      <p>
+      <p className="Vacancy-description">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
