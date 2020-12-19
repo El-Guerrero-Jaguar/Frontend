cd163b2c240c1c7bf028a66f7da6e3221bf316ed
Tonalli López <aguilablanca_7@live.com>
2020-12-19 01:39:37 -0600

    [Update] StudentList styles

--- src/containers/StudentsList.jsx
+++ src/containers/StudentsList.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import profile from '../assets/statics/profile.svg';
 
 function StudentList() {
   return (
     <div className="StudentList_container">
-      <h1> Usuario </h1>
+      <Link className="StudentList_item" to="/vacancies/applied">
+        <img src={profile} alt="User" />
+        <div className="StudentList_item-text">
+          <h3>Nombre del estudiante</h3>
+          <p>Especialidad</p>
+        </div>
+      </Link>
+      <Link className="StudentList_item" to="/vacancies/applied">
+        <img src={profile} alt="User" />
+        <div className="StudentList_item-text">
+          <h3>Nombre del estudiante</h3>
+          <p>Especialidad</p>
+        </div>
+      </Link>
     </div>
   );
 }
