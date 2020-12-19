110ba0c59949318924c66afc7497c58599f598b1
Tonalli López <aguilablanca_7@live.com>
2020-12-18 22:25:13 -0600

    [Add] Connexion to back

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {
+  ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
 import { AppContextProvider } from './context';
 
 import App from './routes/App';
 
+const httpLink = createHttpLink({
+  uri: 'http://ec2-3-21-233-42.us-east-2.compute.amazonaws.com:3000/api',
+});
+
+const linkError = onError(({ graphQLErrors, networkErrors }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, location, path }) => (
+      console.log(`[Graphql Error]: Message: ${message} Location: ${location} Path: ${path}`)
+    ));
+  }
+  if (networkErrors) {
+    console.log(`[Network Error]: ${networkErrors}`);
+  }
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: linkError.concat(httpLink),
+});
+
 render(
   <AppContextProvider>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </AppContextProvider>,
   document.getElementById('app'),
 );
