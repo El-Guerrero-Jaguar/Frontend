df5f6b182700c7537bc61504abaf3bfcaafe54b4
Tonalli López <aguilablanca_7@live.com>
2020-12-19 00:43:47 -0600

    [Update] Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import '../assets/styles/_header.scss';
 
+import logo from '../assets/statics/logoPlatzimaster.png';
+import profile from '../assets/statics/profile.png';
+
 const Header = () => (
-  <div className="container_Header">
-    <h1> Usuario </h1>
-  </div>
+  <header className="Header-container">
+    <Link to="/">
+      <img className="Header-logo" src={logo} alt="Logo" />
+    </Link>
+    <div className="Header-user">
+      <img src={profile} alt="User" />
+      <h1> Usuario </h1>
+    </div>
+  </header>
 );
 
 export default Header;
