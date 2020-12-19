a3756ea9fc07ac7529ee4019a497fe22bd73323d
Tonalli López <aguilablanca_7@live.com>
2020-12-19 00:44:06 -0600

    [Update] SidebarMenu

--- src/containers/SidebarMenu.jsx
+++ src/containers/SidebarMenu.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import IconVacancies from '../assets/statics/vacancies.svg';
+import IconVacanciesApplied from '../assets/statics/vacanciesApplied.svg';
+import IconProfile from '../assets/statics/profile.svg';
+
 function SidebarMenu() {
   return (
     <div className="sidebarmenu_container">
       <nav>
         <ul>
-          <li><NavLink to="/" activeClassName="active" exact>Vacantes</NavLink></li>
-          <li><NavLink to="/vacancies/applied/" activeClassName="active" exact>Mis aplicaciones</NavLink></li>
-          <li><NavLink to="/profile/">Perfil</NavLink></li>
+          <li>
+            <NavLink to="/" activeClassName="active" exact>
+              <img src={IconVacancies} alt="Icon vacancies" />
+              Vacantes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/vacancies/applied/" activeClassName="active" exact>
+              <img src={IconVacanciesApplied} alt="Icon vacancies applied" />
+              Mis aplicaciones
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile/">
+              <img src={IconProfile} alt="Icon profile" />
+              Perfil
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
